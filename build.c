14cfa877bef5ed7b9ea457a01f0087eae3133859
Aaron Thompson <athompsonbriley.56@gmail.com>
2024-09-26 16:11:10 -0400

    Update build.c

--- build.c
+++ build.c
@@ -3,7 +3,6 @@
 int main() {
     const char* source_files[] = {"main.c"};
     CBuildConfig config = cbuild_default_config(source_files, 1, "Syncio.exe");
-    cbuild_add_flags(&config, "-Wall", "-Werror");
     cbuild_run(&config);
     return 0;
 }
