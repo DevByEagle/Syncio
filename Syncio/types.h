29a72bdfc036cdbbe4d59c025626d0f2528318ac
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-09-27 12:25:14 -0400

    Update types.h

--- Syncio/types.h
+++ Syncio/types.h
@@ -1,18 +1,6 @@
 #pragma once
 
-#ifdef __cplusplus
-namespace Syncio {
-    typedef unsigned char Uint8_t;
-    typedef unsigned short Uint16_t;
-    typedef unsigned long Uint32_t;
-    typedef unsigned long long Uint64_t; 
-}
-#else
-typedef unsigned char Uint8_t;
-typedef unsigned short Uint16_t;
-typedef unsigned long Uint32_t;
-typedef unsigned long long Uint64_t;
-typedef Uint8_t UBool;
-#define true (UBool)1
-#define false (UBool)0
-#endif
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned long u32;
+typedef unsigned long long u64;
