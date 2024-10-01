ae1d6710bee27289f746c1a92422d716774c2c04
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-10-01 15:26:13 -0400

    Update build.c

--- build.c
+++ build.c
@@ -3,7 +3,6 @@
 int main() {
     const char* source_files[] = {"main.cpp"};
     CBuildConfig config = cbuild_default_config(source_files, 1, "Syncio.exe");
-    cbuild_add_flags(&config, "-Wall");
     cbuild_set_language_flags(&config, true);
     cbuild_run(&config);
     return 0;
