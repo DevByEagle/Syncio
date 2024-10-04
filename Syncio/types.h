0b0bf5f537d46bd4d169d293dd1d9456f7911301
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-10-04 15:09:34 -0400

    Update types.h

--- Syncio/types.h
+++ Syncio/types.h
@@ -6,14 +6,12 @@ namespace Syncio {
     typedef unsigned short Uint16_t;
     typedef unsigned long Uint32_t;
     typedef unsigned long long Uint64_t;
-    typedef unsigned long Size_t;
 }
 #else
 typedef unsigned char Uint8_t;
 typedef unsigned short Uint16_t;
 typedef unsigned long Uint32_t;
 typedef unsigned long long Uint64_t;
-typedef unsigned long Size_t;
 #endif
 
 // typedef void (*SyncEvent)(void);
