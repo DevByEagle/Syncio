9f550663fe58a412b90c7582b0df0f1775a0e181
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-10-05 20:08:54 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "Syncio/synclib.h"
 
 int main() {
-    Function<int(int, int)> add = [](int a, int b) { return a + b; };
+    Syncio::SyncEvent<int(int, int)> add = [](int a, int b) { return a + b; };
 
     if (add) {  // Check if the function is valid
         std::cout << "Result: " << add(3, 4) << std::endl;  // Output: Result: 7
