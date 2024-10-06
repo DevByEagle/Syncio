71c90ceacb0bd28f2f62ccd8cb19ac36d8006c97
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-10-06 00:46:42 +0000

    Updated Code

--- Syncio/synclib.h
+++ Syncio/synclib.h
@@ -7,13 +7,11 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <stdexcept>
 namespace Syncio {
-    template<typename T>
     class ArrayLite {
     public:
         ArrayLite(size_t initial_capacity) {
-            data = (T*)std::malloc(initial_capacity * sizeof(int));
+            data = (int*)std::malloc(initial_capacity * sizeof(int));
             size = 0;
             capacity = initial_capacity;
         }
@@ -22,28 +20,28 @@ namespace Syncio {
             delete[] data;
         }
 
-        void Append(const T& value) {
+        void Append(int value) {
             if (size >= capacity) {
                 capacity *= 2;
-                data = (T*)std::realloc(data, capacity * sizeof(int));
+                data = (int*)std::realloc(data, capacity * sizeof(int));
             }
             data[size++] = value;
         }
 
-        virtual T Get(size_t index) {
+        virtual int Get(size_t index) {
             if (index < size) {
                 return data[index];
             }
-            throw std::out_of_range("Index out of range");
+            return -1;
         }
 
-        virtual void Set(size_t index, const T& value) {
+        virtual void Set(size_t index, int value) {
             if (index < size) {
                 data[index] = value;
             }
         }
 
-        size_t Size() const {
+        size_t Size() {
             return size;
         }
 
@@ -52,7 +50,7 @@ namespace Syncio {
         }
 
     private:
-        T* data;
+        int* data;
         std::size_t size;
         std::size_t capacity;
     };
