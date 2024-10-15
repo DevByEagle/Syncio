b87d9a4bde86e4e9e9c9cd5df215527e1ebc7388
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-10-14 21:43:00 -0400

    Update types.h

--- Syncio/types.h
+++ Syncio/types.h
@@ -1,16 +1,9 @@
 #pragma once
 
-#define BLOCK_SIZE 64 // Size of the memory block
-
-typedef unsigned char Uint8_t;
-typedef unsigned short Uint16_t;
-typedef unsigned long Uint32_t;
-typedef unsigned long long Uint64_t;
-
 #ifdef __cplusplus
 namespace Syncio {
-    typedef void (*SyncEvent)(void);
+
 }
 #else
-typedef void (*SyncEvent)(void);
+typedef enum { true, false } bool;
 #endif
