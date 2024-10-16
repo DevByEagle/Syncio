507e7949eacf516cf701518a11333951e3427b81
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-10-16 16:27:05 +0000

    Update Code

---
+++ include/libs/stringc.h
@@ -0,0 +1,26 @@
+#ifndef STRINGC_H
+#define STRINGC_H
+
+#include <iostream>
+#include <cstring>
+#include <utility>
+
+namespace Syncio {
+    template<typename CharT>
+    class StringC {
+    public:
+       StringC() : data(nullptr), length (0), isEmpty(true) {}
+       StringC(const CharT* initial) {
+        length = std::strlen(initial);
+        data = new T[length + 1];
+       }
+
+
+    private:
+       CharT* data;
+       std::size_t length;
+       bool isEmpty;
+    };
+}
+
+#endif // STRINGC_H
