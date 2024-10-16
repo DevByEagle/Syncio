b57acb735a493c5aca42e5790ed3f4d6f80e433c
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-10-15 20:28:24 -0400

    Update syncio.c

--- src/syncio.c
+++ src/syncio.c
@@ -13,7 +13,9 @@ void delay(unsigned int ms) {
 void println(string format, ...) {
  va_list args;
  va_start(args, format);
+
  vprintf(format, args);
+    
  va_end(args);   
 }
 
@@ -24,38 +26,4 @@ void raise(string format, ...) {
     vfprintf(stdout, format, args);
 
     va_end(args);
-}
-
-char* input(string prompt) {
-    // Print the prompt
-    printf("%s", prompt);
-
-    // Allocate an initial buffer
-    Size_t buffer_size = 64;
-    char* buffer = malloc(buffer_size);
-    if (!buffer) {
-        return NULL; // Handle allocation failure
-    }
-
-    Size_t length = 0;
-    int ch;
-
-    // Read input character by character
-    while ((ch = getchar()) != '\n' && ch != EOF) {
-        if (length + 1 >= buffer_size) {
-            // Resize buffer if needed
-            buffer_size *= 2;
-            char* new_buffer = realloc(buffer, buffer_size);
-            if (!new_buffer) {
-                free(buffer); // Free the previous buffer
-                return NULL;
-            }
-            buffer = new_buffer;
-        }
-        buffer[length++] = (char)ch;
-    }
-
-    buffer[length] = '\0'; // Null-terminate the string
-
-    return buffer;
 }
