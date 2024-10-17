cd7ff7a8df8d9910bfe5ff7506730e910fd1f1af
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-10-16 22:58:15 -0400

    Update synclib.cpp

--- src/synclib.cpp
+++ src/synclib.cpp
@@ -2,11 +2,6 @@
 #include "../include/synclib.h"
 
 template <typename T>
-T* xalloc(Size_t count) {
-  T* ptr = static_cast<T*>(std::malloc(count * sizeof(T)));
-  if (ptr == nullptr) {
-    raise("Memory allocation failed\n");
-  }
-  return ptr;
+T* Syncio::xalloc(Size_t count) {
+  
 }
-
