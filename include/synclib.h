2cba16a9db5d7ab85c9fd249f2f2a1d90eb2ba59
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-10-16 22:51:49 -0400

    Update synclib.h

--- include/synclib.h
+++ include/synclib.h
@@ -2,18 +2,16 @@
 #define SYNCLIB_H
 
 #if defined(_WIN32) || defined(_WIN64)
+#include "types.h"
 #include "libs/stringc.h"
-#include "../types.h"
 #elif __linux__
-#include <Syncio/stringc.h>
-#include <Syncio/types.h>
+#include "types.h"
+#include "libs/stringc.h"
 #endif
-
-using Syncio::Size_t;
 
 namespace Syncio {
   template <typename T>
-  T* xalloc(Size_t cound);
+  T* xalloc(Size_t count);
 }
 
 #endif // SYNCLIB_H
