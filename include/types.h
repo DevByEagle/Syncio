cf38cae239c6d79bef2c91e1e36dd5843b18ad4a
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-10-17 14:38:28 -0400

    Update types.h

--- include/types.h
+++ include/types.h
@@ -2,11 +2,7 @@
 
 #include "synclib.h"
 
-#ifdef __cplusplus
 namespace Syncio {
     using string = StringC<char>;
     typedef unsigned long Size_t;
 }
-#else
-typedef unsigned long Size_t;
-#endif
