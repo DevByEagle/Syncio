bd0095eb7783ac8bed0ae01d789a5532a9a16774
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-10-16 22:56:13 -0400

    Update synclib.h

--- include/synclib.h
+++ include/synclib.h
@@ -1,13 +1,7 @@
 #ifndef SYNCLIB_H
 #define SYNCLIB_H
 
-#if defined(_WIN32) || defined(_WIN64)
 #include "types.h"
-#include "libs/stringc.h"
-#elif __linux__
-#include "types.h"
-#include "libs/stringc.h"
-#endif
 
 namespace Syncio {
   template <typename T>
