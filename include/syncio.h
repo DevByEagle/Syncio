29b3737862eb6ee5969d16bbdb358902a17e73a8
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-10-16 23:39:47 -0400

    Update syncio.h

--- include/syncio.h
+++ include/syncio.h
@@ -11,10 +11,9 @@
 #include "types.h"
 
 namespace Syncio {
-    namespace System {
         /** @brief Outputs a message to the console and appends a newline for better readability. */
         void println(string format, ...);
-        
+            
         /** @brief Triggers an error, throwing an exception or terminating the program with a specified message. */
         void raise(string format, ...);
     }
