a05b40e5cfa1fb074185136e4889bd4086768f6c
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-10-16 23:47:14 -0400

    Update synclib.h

--- include/synclib.h
+++ include/synclib.h
@@ -2,7 +2,12 @@
 #define SYNCLIB_H
 
 #include "libs/stringc.h"
+
 namespace Syncio {
+  namespace Foundation {
+    template <typename CharT>
+    class base_string : public StringC<CharT>;
+  }
 }
 
 #endif // SYNCLIB_H
