1ee6c46525c29e7bf9e2baa0607103b951c8ff76
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-10-19 20:38:23 -0400

    Update syncio.h

--- include/syncio.h
+++ include/syncio.h
@@ -13,6 +13,8 @@ void println(const char* format, ...);
  /** @brief Triggers an error, throwing an exception or terminating the program with a specified message. */
 void raise(const char* format, ...);
 
+char* fload(const char* filename);
+
 #ifdef __cplusplus
 }
 #endif
